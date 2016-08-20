6f8006ae92cf46abe09ea0d5bc884668f8a47d91
jaspercurry <jaspercurry@gmail.com>
2016-08-19 21:37:43 -0400

    made lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction)}
+  validate :clickval
+
+  def clickval
+
+    if title != nil
+      conditions = [/Won't\sBelieve/, /Secret/, /Guess/, /Top \d/]
+        conditions.each do |cond|
+          if title.match(cond) != nil
+            return true
+          end
+        end
+        errors.add(:non_clickbait, 'is invalid if not clickbait')
+    end
+  end
 end
